e5602b2eb7037b5fe9142dc924890ef418795b96
albino kira <albino.kira@gmail.com>
2026-08-22 14:24:20 -0300

    feat(reconhecimento): catalogo completo de marcas, modelos e eletricos v1.75

--- android/app/src/main/assets/app.js
+++ android/app/src/main/assets/app.js
@@ -23,7 +23,7 @@ function homeLogout() {
 }
 window.homeLogout = homeLogout;
 
-const CURRENT_APP_VERSION = 'v1.74';
+const CURRENT_APP_VERSION = 'v1.75';
 
 async function checkForSystemUpdates(showFeedback = false) {
   const versionEl = document.getElementById('systemAppVersionDisplay') || document.getElementById('systemVersionText');
@@ -6360,6 +6360,123 @@ window.vpSetVehicleType = function(type, forceRender = true) {
   }
 };
 
+function vpDetectVehicleTypeFromText(rawText) {
+  if (!rawText || typeof rawText !== 'string') return 'carro';
+  const text = (' ' + rawText.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') + ' ')
+    .replace(/[^A-Z0-9\.\-\/]/g, ' ')
+    .replace(/\s+/g, ' ');
+
+  // 1. REGRAS ESTRITAS DE MOTO (Modelos e Marcas)
+  const motoStrictTokens = [
+    'MOTOCICLETA', 'MOTO', 'MOTONETA', 'SCOOTER', 'CICLOMOTOR', 'TRICICLO',
+    // Honda Motos
+    'TITAN', 'FAN', 'START', 'BIZ', 'POP110', 'POP 110', 'POP100', 'POP 100',
+    'BROS', 'NXR', 'XRE', 'XRE300', 'XRE 300', 'XRE190', 'XRE 190',
+    'SAHARA', 'TORNADO', 'TWISTER', 'CB300', 'CB 300', 'CB 300F', 'CB300F',
+    'CB500', 'CB 500', 'CB650', 'CB 650', 'CB1000', 'CB 1000', 'CBR', 'CBR600', 'CBR1000',
+    'NC750', 'NC 750', 'NC700', 'TRANSALP', 'AFRICA TWIN', 'HORNET',
+    'PCX', 'ADV150', 'ADV 150', 'ADV350', 'ADV 350', 'LEAD 110', 'SH150', 'SH 150', 'SH300', 'SH 300', 'FORZA',
+    'CG160', 'CG 160', 'CG150', 'CG 150', 'CG125', 'CG 125', 'CG CARGO', 'FALCON', 'NX4', 'SHADOW',
+    // Yamaha Motos
+    'FAZER', 'FZ15', 'FZ 15', 'FZ25', 'FZ 25', 'FZ6', 'FAZER 250', 'FAZER 150',
+    'FACTOR', 'FACTOR 150', 'FACTOR 125', 'LANDER', 'XTZ', 'XTZ 250', 'XTZ 150', 'CROSSER',
+    'TENERE', 'TENERE 250', 'TENERE 700', 'T7', 'MT-03', 'MT-07', 'MT-09', 'MT-10', 'MT03', 'MT07', 'MT09', 'MT10',
+    'YZF-R3', 'R3', 'R15', 'R1', 'R6', 'NMAX', 'XMAX', 'FLUO', 'NEO 125', 'CRYPTON', 'YBR', 'VIRAGO', 'DRAGSTAR', 'MIDNIGHT STAR', 'RD 135', 'RD 350', 'DT 200',
+    // Suzuki Motos
+    'BURGMAN', 'INTRUDER', 'YES 125', 'GSR 750', 'GSR 150', 'GSX-S', 'GSX-R', 'GSX', 'HAYABUSA', 'BANDIT', 'V-STROM', 'VSTROM', 'BOULEVARD', 'INAZUMA', 'GLADIUS',
+    // Kawasaki
+    'NINJA', 'NINJA 300', 'NINJA 400', 'NINJA 650', 'NINJA 1000', 'ZX-6R', 'ZX-10R', 'ZX6R', 'ZX10R', 'Z400', 'Z 400', 'Z650', 'Z 650', 'Z900', 'Z 900', 'Z1000',
+    'VERSYS', 'VERSYS 300', 'VERSYS 650', 'VERSYS 1000', 'VULCAN', 'KLX', 'D-TRACKER',
+    // BMW Motos
+    'GS 1250', 'GS 1200', 'GS 1300', 'GS 850', 'GS 750', 'GS 310', 'G 310', 'G310GS', 'G310R', 'F850GS', 'F800GS', 'F750GS', 'F900R', 'F900XR', 'S1000RR', 'S1000R', 'R1250GS', 'R1200GS', 'R1300GS', 'R NINET',
+    // Triumph
+    'TIGER', 'TIGER 900', 'TIGER 1200', 'TIGER 800', 'TIGER 660', 'BONNEVILLE', 'STREET TRIPLE', 'SPEED TRIPLE', 'TRIDENT', 'ROCKET 3', 'THRUXTON', 'SPEED TWIN',
+    // Royal Enfield
+    'HUNTER 350', 'HUNTER', 'METEOR 350', 'METEOR', 'CLASSIC 350', 'HIMALAYAN', 'SCRAM 411', 'INTERCEPTOR 650', 'INTERCEPTOR', 'CONTINENTAL GT', 'SUPER METEOR', 'SHOTGUN 650',
+    // Ducati
+    'PANIGALE', 'MULTISTRADA', 'DIAVEL', 'HYPERMOTARD', 'DESERTX', 'MONSTER 797', 'MONSTER 821', 'MONSTER 1200', 'STREETFIGHTER',
+    // Harley Davidson
+    'FAT BOY', 'HERITAGE', 'IRON 883', 'SPORTSTER', 'ROAD KING', 'STREET GLIDE', 'ULTRA LIMITED', 'BREAKOUT', 'NIGHTSTER', 'LOW RIDER', 'FAT BOB',
+    // KTM
+    'KTM DUKE', 'DUKE 200', 'DUKE 390', 'DUKE 790', 'DUKE 890', 'ADVENTURE 390', 'ADVENTURE 890', 'ADVENTURE 1290', 'KTM 250', 'KTM 300',
+    // Bajaj / Haojue / Dafra / Shineray
+    'DOMINAR', 'DOMINAR 400', 'DOMINAR 200', 'DOMINAR 160', 'PULSAR', 'CHETAK',
+    'DR160', 'DR 160', 'CHOPPER ROAD', 'MASTER RIDE', 'DK150', 'DK 150', 'VR150', 'LINDY 125', 'NK150',
+    'CITYCOM', 'MAXSYM', 'APACHE RTR', 'NH 190', 'NH 300', 'CRUISYM', 'HORIZON 150', 'HORIZON 250',
+    'VOLTZ', 'EVS', 'EV1', 'WATTS W125', 'SUPER SOCO', 'MUVINX',
+    'JET 50', 'JET 125', 'PHOENIX 50', 'PHOENIX S', 'WORKER 125', 'SHINERAY XY', 'XY 50'
+  ];
+
+  const motoExclusiveBrands = [
+    'TRIUMPH', 'DUCATI', 'ROYAL ENFIELD', 'HARLEY-DAVIDSON', 'HARLEY DAVIDSON', 'HARLEY',
+    'BAJAJ', 'HAOJUE', 'DAFRA', 'SHINERAY', 'KASINSKI', 'SUNDOWN', 'TRAXX', 'VESPA', 'PIAGGIO',
+    'MV AGUSTA', 'APRILIA', 'BENELLI', 'KTM', 'VOLTZ', 'SUPER SOCO'
+  ];
+
+  // 2. REGRAS ESTRITAS DE CAMINHÃO
+  const caminhaoStrictTokens = [
+    'CAMINHAO', 'CAMINHÃO', 'CAVALO MECANICO', 'CAVALO MECÂNICO', 'CAVALO-MECANICO',
+    'BITREM', 'RODOTREM', 'CARRETA', 'SEMI-REBOQUE', 'SEMIRREBOQUE', 'BASCULANTE',
+    'BITRUCK', 'TRUCK', 'CHASSI', 'CACAMBA', 'CAÇAMBA', 'GRANELEIRO', 'SIDER',
+    // Scania
+    'SCANIA', 'R440', 'R450', 'R480', 'R500', 'R540', 'R560', 'R620',
+    'P310', 'P360', 'P250', 'P270', 'P280', 'G440', 'G420', 'G400', 'G380', '113H', '113', '124G', '124', '112', '142', '143',
+    // Volvo Caminhões
+    'VOLVO FH', 'VOLVO FM', 'VOLVO FMX', 'VOLVO VM', 'FH 460', 'FH 500', 'FH 540', 'FH 420', 'FH 440', 'FH 520',
+    'FM 370', 'FM 380', 'FM 400', 'FM 440', 'VM 260', 'VM 270', 'VM 330', 'VM 310', 'VM 210', 'VM 220',
+    // Mercedes Caminhões
+    'ACTROS', 'ATEGO', 'ACCELO', 'AXOR', 'ATRON', 'AROCS',
+    'MB 1620', 'MB 1938', 'MB 1113', 'MB 1313', 'MB 710', 'MB 1935', 'MB 1944', 'MB 2544', 'MB 2546', 'MB 2646', 'MB 2651',
+    '1620', '1938', '1113', '1313', '710', '2544', '2546', '2646', '2651', '2548', '2426', '2428', '1719', '1726', '815', '1016',
+    // VW Caminhões
+    'CONSTELLATION', 'DELIVERY', 'METEOR', 'WORKER', 'TITAN TRACTOR',
+    '24.250', '24.280', '25.440', '25.460', '26.460', '26.510', '31.320', '19.320', '19.360', '17.190', '17.250', '17.280', '15.180', '15.190', '13.180', '13.190', '9.150', '8.150', '10.160', '11.180', '6.160', '9.170',
+    // Iveco Caminhões
+    'STRALIS', 'HI-WAY', 'HI-ROAD', 'TECTOR', 'S-WAY', 'VERTIS', 'EUROCARGO', 'CAVALLINO', 'CURSOR', 'DAILY 70C', 'DAILY 35S',
+    // DAF Caminhões
+    'DAF XF', 'DAF CF', 'DAF LF', 'XF105', 'XF 105', 'XF 480', 'XF 530', 'CF 85',
+    // Ford Caminhões
+    'FORD CARGO', 'CARGO 815', 'CARGO 2428', 'CARGO 2422', 'CARGO 2429', 'CARGO 1722', 'CARGO 1719', 'CARGO 1932', 'CARGO 2842', 'CARGO 2042', 'CARGO 1119', 'F-4000', 'F4000', 'F-12000', 'F-14000', 'F-11000', 'F-16000',
+    // MAN Caminhões
+    'MAN TGX', 'MAN TGS', 'TGX 28.440', 'TGX 29.440', 'TGX 29.480'
+  ];
+
+  // 3. REGRAS ESTRITAS DE PICAPE
+  const picapeStrictTokens = [
+    'HILUX', 'HILUX CD', 'HILUX CS', 'HILUX SR', 'HILUX SRV', 'HILUX SRX', 'HILUX GR-S', 'HILUX STD', 'TACOMA', 'TUNDRA',
+    'S10', 'S-10', 'MONTANA', 'SILVERADO', 'D20', 'D-20', 'D10', 'C10', 'C20', 'COLORADO',
+    'RANGER', 'RANGER XLS', 'RANGER XLT', 'RANGER LIMITED', 'RANGER RAPTOR', 'RANGER BLACK', 'RANGER STORM', 'MAVERICK', 'F-150', 'F150', 'F-250', 'F250', 'COURIER',
+    'STRADA', 'STRADA FREEDOM', 'STRADA VOLCANO', 'STRADA RANCH', 'STRADA ULTRA', 'STRADA ENDURANCE', 'STRADA WORKING', 'STRADA ADVENTURE',
+    'TORO', 'TORO FREEDOM', 'TORO VOLCANO', 'TORO RANCH', 'TORO ULTRA', 'TORO ENDURANCE', 'TITANO', 'FIORINO',
+    'SAVEIRO', 'SAVEIRO CROSS', 'SAVEIRO ROBUST', 'SAVEIRO TRENDLINE', 'SAVEIRO SURF', 'SAVEIRO PEPPER', 'AMAROK', 'AMAROK V6', 'AMAROK HIGHLINE', 'AMAROK COMFORTLINE', 'AMAROK EXTREME',
+    'FRONTIER', 'FRONTIER PRO4X', 'FRONTIER ATTACK', 'FRONTIER XE', 'FRONTIER LE', 'FRONTIER S', 'FRONTIER PLATINUM',
+    'L200', 'L200 TRITON', 'TRITON SPORT', 'TRITON SAVANA', 'TRITON HPE', 'TRITON GLS', 'TRITON GLX',
+    'RAMPAGE', 'RAM 1500', 'RAM 2500', 'RAM 3500', 'RAM CLASSIC', 'RAM REBEL', 'RAM LARAMIE', 'RAM BIG HORN',
+    'OROCH', 'DUSTER OROCH', 'OROCH OUTSIDER', 'OROCH PRO',
+    'LANDTREK', 'HOGGAR',
+    'JAC HUNTER', 'JAC T6', 'JAC T8', 'JAC T9', 'BYD SHARK', 'GWM POER',
+    'CABINE DUPLA', 'CABINE SIMPLES', 'CABINE ESTENDIDA', 'CD 4X4', 'CS 4X4', 'PICKUP', 'PICK-UP', 'PICAPE'
+  ];
+
+  const hasToken = (token) => {
+    const cleanToken = (' ' + token.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') + ' ')
+      .replace(/[^A-Z0-9\.\-\/]/g, ' ')
+      .replace(/\s+/g, ' ');
+    return text.includes(cleanToken.trim());
+  };
+
+  if (motoStrictTokens.some(t => hasToken(t))) return 'moto';
+  if (motoExclusiveBrands.some(b => hasToken(b))) return 'moto';
+  if (caminhaoStrictTokens.some(t => hasToken(t))) return 'caminhao';
+  if (picapeStrictTokens.some(t => hasToken(t))) return 'picape';
+
+  const currentSurveyType = document.getElementById('typeInput')?.value || '';
+  if (currentSurveyType === 'Moto') return 'moto';
+
+  return 'carro';
+}
+window.vpDetectVehicleTypeFromText = vpDetectVehicleTypeFromText;
+
 window.openVehiclePartsModal = function() {
   vpLoadState();
   if (typeof window.vpSyncCatalogWithCloud === 'function') {
@@ -6370,46 +6487,10 @@ window.openVehiclePartsModal = function() {
   const vehicleTitle = plateInput ? plateInput.value.trim() : '';
   vpCurrentVehicleModelName = vehicleTitle;
 
-  const vUpper = vehicleTitle.toUpperCase();
   const titleDisplay = document.getElementById('vpVehicleTitle');
 
-  // Auto-detecção inteligente de tipo com keywords ampliadas
-  let detectedType = 'carro';
-
-  const motoKeywords = [
-    'MOTO', 'MOTOCICLETA', 'HONDA', 'YAMAHA', 'SUZUKI', 'KAWASAKI', 'BMW GS', 'TRIUMPH', 
-    'DUCATI', 'ROYAL ENFIELD', 'SHINERAY', 'HAOJUE', 'DAFRA', 'TWISTER', 'CB ', 'CB300', 
-    'CB 300', 'CB500', 'CB 500', 'CB650', 'CB 650', 'CB1000', 'CBR', 'CG ', 'CG160', 
-    'CG 160', 'CG150', 'CG 150', 'CG125', 'TITAN', 'FAN', 'START', 'BIZ', 'POP 110', 
-    'POP 100', 'POP110', 'POP100', 'BROS', 'NXR', 'XRE', 'SAHARA', 'TORNADO', 'FAZER', 
-    'FZ15', 'FZ 15', 'FZ25', 'FZ 25', 'FZ6', 'LANDER', 'XTZ', 'TENERE', 'TÉNÉRÉ', 
-    'FACTOR', 'CROSSER', 'MT-03', 'MT-07', 'MT-09', 'MT03', 'MT07', 'MT09', 'R3', 
-    'R1', 'PCX', 'NMAX', 'XMAX', 'ELITE', 'NEO', 'ADV 150', 'ADV 350', 'SH 150', 
-    'SH 300', 'BURGMAN', 'INTRUDER', 'TIGER', 'Z400', 'Z900', 'NINJA', 'VERSYS', 
-    'SCRAMBLER', 'METEOR', 'HUNTER', 'CLASSIC 350', 'HIMALAYAN'
-  ];
-
-  const caminhaoKeywords = [
-    'CAMINHAO', 'CAMINHÃO', 'CAVALO', 'TRUCK', 'BITREM', 'RODOTREM', 'CARRETA', 'SCANIA', 
-    'VOLVO FH', 'VOLVO FM', 'VOLVO VM', 'VOLVO FMX', 'MERCEDES-BENZ', 'MERCEDES BENZ', 
-    'MB ', 'ACTROS', 'ATEGO', 'ACCELO', 'AXOR', '1620', '1938', '1113', '1313', 'VW 24.', 
-    'CONSTELLATION', 'DELIVERY', 'METEOR', 'MAN ', 'IVECO', 'STRALIS', 'HI-WAY', 
-    'TECTOR', 'DAILY', 'S-WAY', 'DAF', 'XF', 'CF', 'FORD CARGO', 'CARGO ', 'F-4000', 
-    'F-12000', 'F-14000', 'CAVALO MECANICO', 'CAVALO MECÂNICO'
-  ];
-
-  const picapeKeywords = [
-    'HILUX', 'S10', 'RANGER', 'TORO', 'STRADA', 'SAVEIRO', 'AMAROK', 'MONTANA', 
-    'OROCH', 'FRONTIER', 'L200', 'RAM ', 'RAMPAGE', 'TITANO', 'MAVERICK', 'FIORINO', 'COURIER'
-  ];
-
-  if (motoKeywords.some(k => vUpper.includes(k))) {
-    detectedType = 'moto';
-  } else if (caminhaoKeywords.some(k => vUpper.includes(k))) {
-    detectedType = 'caminhao';
-  } else if (picapeKeywords.some(k => vUpper.includes(k))) {
-    detectedType = 'picape';
-  }
+  // Auto-detecção inteligente de tipo com catálogo completo de marcas e modelos
+  const detectedType = vpDetectVehicleTypeFromText(vehicleTitle);
 
   // Aplica o tipo detectado e atualiza as abas
   window.vpSetVehicleType(detectedType, false);
@@ -7133,5 +7214,25 @@ function vpEscapeHtml(str) {
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
+}
+
+// Sincronização em tempo real da digitação da placa / modelo com o detector de veículo
+if (typeof plateInput !== 'undefined' && plateInput) {
+  plateInput.addEventListener('input', () => {
+    const text = plateInput.value.trim();
+    const type = vpDetectVehicleTypeFromText(text);
+    const btnSpan = document.querySelector('.btn-open-parts-selector span');
+    if (btnSpan) {
+      if (type === 'moto') {
+        btnSpan.textContent = '🏍️ Selecionar Partes da Moto (Zonas)';
+      } else if (type === 'caminhao') {
+        btnSpan.textContent = '🚛 Selecionar Partes do Caminhão (Zonas)';
+      } else if (type === 'picape') {
+        btnSpan.textContent = '🛻 Selecionar Partes da Picape (Zonas)';
+      } else {
+        btnSpan.textContent = '🚗 Selecionar Partes do Veículo (Zonas)';
+      }
+    }
+  });
 }
 
